b013f76d10106db35dab1e171ad965468dc42b50
Taig4 <xTaig4@gmail.com>
2025-06-22 04:34:16 +0200

    removed prefab, changed ParallexEffect from single backgrounds to multiple
    ParallexEffect now handles multiple backgrounds, making managing parallex effect easier

--- Assets/Scripts/ParallexEffect.cs
+++ Assets/Scripts/ParallexEffect.cs
@@ -4,36 +4,59 @@ using UnityEngine;
 
 public class ParallexEffect : MonoBehaviour
 {
+
     [SerializeField] private float scrollSpeed = 1f;
-    [SerializeField] float parallaxMultiplier = 0.2f;
-    private Vector3 startPosition;
 
+    [SerializeField] float parallaxMultiplier = 0.3f;
 
-    //[SerializeField] float parallaxEffectMultiplier1 = 0.4f;
-    //[SerializeField] float parallaxEffectMultiplier2 = 0.6f;
-    //[SerializeField] float parallaxEffectMultiplier3 = 0.8f;
-    //[SerializeField] float parallaxEffectMultiplier4 = 1f;
+    public float[] parallaxMultipliers = new float[5];
 
-
-    //List<GameObject> backgrounds;
+    private GameObject parent;
+    private List<GameObject> backgrounds;
+    private List<Vector2> backgroundPositions;
+    private Vector2[] startPositions = new Vector2[5];
 
     void Start()
     {
-        startPosition = transform.position;
+        
+        parent = this.gameObject;
+        backgrounds = GetAllChildren(parent);
+        backgroundPositions = new List<Vector2>();
 
-        //backgrounds = new List<GameObject>();
-        //backgrounds.AddRange(GameObject.FindGameObjectsWithTag("Background"));
+        for (int i = 0; i < backgrounds.Count; i++)
+        {
+            backgroundPositions.Add(backgrounds[i].transform.position);
+            startPositions[i] = backgrounds[i].transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float xMovement = Time.time * scrollSpeed;
+        
+        foreach (GameObject background in backgrounds)
+        {
+            Vector2 backgroundPosition = background.transform.position;
+            int index = backgrounds.IndexOf(background);
+            if (index < parallaxMultipliers.Length)
+            {
+                float multiplier = parallaxMultipliers[index];
+                backgroundPosition.x = startPositions[index].x - xMovement * multiplier;
+                background.transform.position = backgroundPosition;
+            }
+        }
 
+        Debug.Log("xMovement: " + xMovement);
+    }
 
-        float xMovement = Time.time * scrollSpeed * parallaxMultiplier;
-        //float yMovement = Time.time * scrollSpeed * parallaxMultiplier.y;
-
-        transform.position = new Vector3(startPosition.x - xMovement, startPosition.y, startPosition.z);
-
+    public List<GameObject> GetAllChildren(GameObject parent)
+    {
+        List<GameObject> children = new List<GameObject>();
+        foreach (Transform child in parent.transform)
+        {
+            children.Add(child.gameObject);
+        }
+        return children;
     }
 }
