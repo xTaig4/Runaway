1a93b16df33675e13023c6c64cf23561eacf0c51
Taig4 <xTaig4@gmail.com>
2025-08-26 04:30:53 +0200

    Refactor parallax effect script for backgrounds and added infinite scrolling
    Renamed class to ParallaxEffect and refactored logic to use an array of background transforms. Improved parallax movement and added infinite scrolling functionality for backgrounds based on camera position.

--- Assets/Scripts/ParallexEffect.cs
+++ Assets/Scripts/ParallexEffect.cs
@@ -2,65 +2,58 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ParallexEffect : MonoBehaviour
+public class ParallaxEffect : MonoBehaviour
 {
-
-    [SerializeField] private float scrollSpeed = 1f;
-
-    [SerializeField] float parallaxMultiplier = 0.3f;
-
-    public float[] parallaxMultipliers = new float[5];
-
-    public bool moveBackgrounds = true;
-
-    private GameObject parent;
-    private List<GameObject> backgrounds;
-    private List<Vector2> backgroundPositions;
-    private Vector2[] startPositions = new Vector2[5];
-    private Camera mainCamera;
+    public Transform[] backgrounds; // Array of background transforms
+    public float parallaxMultiplier = 0.5f;
+    private Camera cam;
+    private float lastCamX;
+    private float[] backgroundsWidth;
 
     void Start()
     {
-        mainCamera = Camera.main;
-        parent = this.gameObject;
-        backgrounds = GetAllChildren(parent);
-        backgroundPositions = new List<Vector2>();
-
-        for (int i = 0; i < backgrounds.Count; i++)
+        cam = Camera.main;
+        lastCamX = cam.transform.position.x;
+        backgroundsWidth = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
         {
-            backgroundPositions.Add(backgrounds[i].transform.position);
-            if (moveBackgrounds)
-            {
-                startPositions[i] = backgrounds[i].transform.position;
-            }
+            backgroundsWidth[i] = backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float xMovement = Time.time * scrollSpeed;
-        
-        foreach (GameObject background in backgrounds)
+
+        float deltaMovement = cam.transform.position.x - lastCamX;
+
+        for (int i = 0; i < backgrounds.Length; i++)
         {
-            Vector2 backgroundPosition = background.transform.position;
-            int index = backgrounds.IndexOf(background);
-            if (index < parallaxMultipliers.Length)
+            // Parallax movement
+            backgrounds[i].position = new Vector3(backgrounds[i].position.x - deltaMovement * parallaxMultiplier, backgrounds[i].position.y, backgrounds[i].position.z);
+            lastCamX = cam.transform.position.x;
+            // Infinite scrolling
+            float leftEdge = backgrounds[i].position.x + (backgroundsWidth[i] * 2);
+            if (cam.transform.position.x > leftEdge)
             {
-                float multiplier = parallaxMultipliers[index];
-                backgroundPosition.x = startPositions[index].x - xMovement * multiplier;
-                background.transform.position = backgroundPosition;
+                // Move to the rightmost position
+                float rightmostX = backgrounds[GetRightmostIndex()].position.x + backgroundsWidth[GetRightmostIndex()];
+                backgrounds[i].position = new Vector3(rightmostX, backgrounds[i].position.y, backgrounds[i].position.z);
             }
         }
     }
 
-    public List<GameObject> GetAllChildren(GameObject parent)
+    int GetRightmostIndex()
     {
-        List<GameObject> children = new List<GameObject>();
-        foreach (Transform child in parent.transform)
+        int index = 0;
+        float maxX = backgrounds[0].position.x;
+        for (int i = 1; i < backgrounds.Length; i++)
         {
-            children.Add(child.gameObject);
+            if (backgrounds[i].position.x > maxX)
+            {
+                maxX = backgrounds[i].position.x;
+                index = i;
+            }
         }
-        return children;
+        return index;
     }
 }
